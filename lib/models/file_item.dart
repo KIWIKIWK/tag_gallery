ce9a664f24ce88a2e00bbc4ca299b3164a06d2c6
KIWIK <wnakf5984@gmail.com>
2024-12-28 23:38:37 +0900

    add FileItem getter

--- lib/models/file_item.dart
+++ lib/models/file_item.dart
@@ -8,5 +8,12 @@ class FileItem{
 
   String get fileName => file.path.split('/').last.split('.').first;
   String get fileExtension => file.path.split('.').last;
+  String get filePath{
+    final split = file.path.split('/');
+    split.removeLast();
+    return "${split.join('/')}/";
+  }
+  DateTime get modifiedDate => file.lastModifiedSync();
+  int get fileSize => file.statSync().size;
   String get contractionFileName => fileName.length > 15 ? "${fileName.substring(0,15).trim()}..." : fileName;
 }
